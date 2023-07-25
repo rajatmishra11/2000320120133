92af969cc59a2137262a45893c6d9d1daeef97f5
Rajat Mishra <rajatmishra482001@gmail.com>
2023-07-25 16:29:26 +0530

    3rd commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import styles from "./index.css";
+
+const Book = () => {
+  return (
+    <article>
+      <img
+        src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR1tXph6SVIgQBULTiQzm3ITCpVIq2W3rLUxHFrSbErsWhznkI1"
+        height={150}
+        width={90}
+        alt="Don quixot"
+      />
+      <h2>Don quixot</h2>
+      <h4>Miguelde cervantes</h4>
+    </article>
+  );
+};
 
 const BookList = () => {
   return (
-    <article>
-      <h2>Book Image Placeholder</h2>
-      <h2>Book title</h2>
-      <h2>Author name</h2>
-    </article>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
-// root.render(<Fun1/>)
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-const BookList = () => {
-    return (
-        <article>
-            <h2>Book Image Placeholder</h2>
-            <h2>Book title</h2>
-            <h2>Author name</h2>
-        </article>
-    )
-};
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BookList />)
-// root.render(<Fun1/>)
